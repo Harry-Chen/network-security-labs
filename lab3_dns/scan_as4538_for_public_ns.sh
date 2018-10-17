0d7c24a15987301edc836dab5afe65520b87ba14
Wende Tan <twd2@163.com>
2018-10-17 11:23:38 +0800

    lab3: bugfix

--- lab3_dns/scan_as4538_for_public_ns.sh
+++ lab3_dns/scan_as4538_for_public_ns.sh
@@ -1,1 +1,1 @@
-cat as4538_prefix4.txt | parallel -j 24 sudo nmap -T5 -n -Pn -sn --script=./dns-query.nse -oN data/public_ns/\`date +%s%N\` {}
+cat data/as4538_prefix4.txt | parallel -j 24 sudo nmap -T5 -n -Pn -sn --script=./dns-query.nse -oN data/public_ns/\`date +%s%N\` {}
