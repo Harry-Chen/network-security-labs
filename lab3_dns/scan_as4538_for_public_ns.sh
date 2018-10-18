b8023457c53f04c7687b849622d5b4dde0d3ae2b
Harry Chen <i@harrychen.xyz>
2018-10-18 08:52:18 +0800

    Rename code file
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- lab3_dns/scan_as4538_for_public_ns.sh
+++ lab3_dns/scan_as4538_for_public_ns.sh
@@ -1,1 +1,2 @@
-cat data/as4538_prefix4.txt | parallel -j 24 sudo nmap -T5 -n -Pn -sn --script=./dns-query.nse -oN data/public_ns/\`date +%s%N\` {}
+mkdir -p data/public_ns/
+cat data/as4538_prefix4.txt | parallel -j 24 sudo nmap -T5 -n -Pn -sn --script=./test_dns_query.nse -oN data/public_ns/\`date +%s%N\` {}
