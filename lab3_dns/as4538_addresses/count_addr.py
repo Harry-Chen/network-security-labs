62509944f9672b24e4577096119afe59c53d5ef2
Wende Tan <twd2@163.com>
2018-10-23 13:57:28 +0800

    lab3: bugfix

--- lab3_dns/as4538_addresses/count_addr.py
+++ lab3_dns/as4538_addresses/count_addr.py
@@ -5,7 +5,7 @@ def readfile(filename):
         return list(map(lambda s: ipaddress.ip_network(s.strip()), f))
 
 count = 0
-for ipn in readfile('as4538_prefix4副本.txt'):
+for ipn in readfile('as4538_prefix4.txt'):
     count += ipn.num_addresses
     print(ipn.num_addresses)
     """
