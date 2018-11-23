59e2d031673c5f5f52fdaaeb6b8df519758f7460
Wende Tan <twd2@163.com>
2018-11-23 11:31:15 +0800

    lab4: data

---
+++ lab4_tunet/auth_base64.py
@@ -0,0 +1,14 @@
+import base64
+
+ORIG = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+AUTH = 'LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA='
+
+auth2orig = dict(zip(AUTH, ORIG))
+orig2auth = dict(zip(ORIG, AUTH))
+
+def encode(data):
+    return ''.join(map(lambda c: orig2auth[c], base64.b64encode(data).decode()))
+
+def decode(str):
+    return base64.b64decode(''.join(map(lambda c: auth2orig[c], str)))
+
